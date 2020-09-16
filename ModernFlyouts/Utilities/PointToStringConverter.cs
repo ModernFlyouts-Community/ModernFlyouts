a5a60ef47ca614914c1711985c07718fbadc9c54
Shankar <shankarbus04082001@hotmail.com>
2020-09-16 18:38:42 +0530

    New features
    1. Added support for saving flyout's position
    2. Implemented Smooth scrolling in Settings Window

---
+++ ModernFlyouts/Utilities/PointToStringConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace ModernFlyouts.Utilities
+{
+    public class PointToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var point = (Point)value;
+
+            return point.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var point = Point.Parse(value.ToString());
+            return point;
+        }
+    }
+}
