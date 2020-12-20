3a3c9a7ead5ced48cfddcf2e46b787d38fe195ec
Sam <samuelgilliessmith@hotmail.com>
2020-12-20 14:42:08 +1300

    Add Flyout Position navigation Header

--- ModernFlyouts/Properties/Strings.Designer.cs
+++ ModernFlyouts/Properties/Strings.Designer.cs
@@ -19,7 +19,7 @@ namespace ModernFlyouts.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Strings {
@@ -732,6 +732,15 @@ namespace ModernFlyouts.Properties {
         public static string Settings_FlyoutDefaultPosition {
             get {
                 return ResourceManager.GetString("Settings.FlyoutDefaultPosition", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Flyout Position.
+        /// </summary>
+        public static string Settings_FlyoutPosition {
+            get {
+                return ResourceManager.GetString("Settings.FlyoutPosition", resourceCulture);
             }
         }
         
