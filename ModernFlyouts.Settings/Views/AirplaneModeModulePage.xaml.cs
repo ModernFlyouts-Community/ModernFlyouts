3aa2911e20c5c56bee42d863136f38bc6195cf63
Sam <samuelg12321@hotmail.com>
2022-01-10 10:04:12 +1300

    Merge Settings, suppress warnings

---
+++ ModernFlyouts.Settings/Views/AirplaneModeModulePage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using ModernFlyouts.Settings.ViewModels;
+
+using Windows.UI.Xaml.Controls;
+
+namespace ModernFlyouts.Settings.Views
+{
+    public sealed partial class AirplaneModeModulePage : Page
+    {
+        public AirplaneModeModuleViewModel ViewModel { get; } = new AirplaneModeModuleViewModel();
+
+        public AirplaneModeModulePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
