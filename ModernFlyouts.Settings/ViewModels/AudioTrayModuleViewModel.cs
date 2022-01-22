3e56e5d8788c53d5db0f0961c579cdbf1cfabb8f
Sam <samuelg12321@hotmail.com>
2022-01-23 12:22:17 +1300

    Updates

--- ModernFlyouts.Settings/ViewModels/AudioTrayModuleViewModel.cs
+++ ModernFlyouts.Settings/ViewModels/AudioTrayModuleViewModel.cs
@@ -9,5 +9,26 @@ namespace ModernFlyouts.Settings.ViewModels
         public AudioTrayModuleViewModel()
         {
         }
+        private bool _isEnabled;
+
+        public bool IsEnabled
+        {
+            get => _isEnabled;
+            set
+            {
+                if (_isEnabled != value)
+                {
+                    _isEnabled = value;
+                    OnPropertyChanged(nameof(IsEnabled));
+
+                    throw new NotImplementedException();
+                    //// Set the status of ColorPicker in the general settings
+                    //GeneralSettingsConfig.Enabled.ColorPicker = value;
+                    //var outgoing = new OutGoingGeneralSettings(GeneralSettingsConfig);
+
+                    //SendConfigMSG(outgoing.ToString());
+                }
+            }
+        }
     }
 }
