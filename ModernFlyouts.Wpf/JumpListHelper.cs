bd60c706847e481b9a277efd4e5e587c0c55a1f4
Sam <samuelg12321@hotmail.com>
2022-01-24 21:31:46 +1300

    More Settings Updates

---
+++ ModernFlyouts.Wpf/JumpListHelper.cs
@@ -0,0 +1,54 @@
+﻿using System.IO;
+using System.Windows.Shell;
+
+namespace ModernFlyouts.Wpf
+{
+    public class JumpListHelper
+    {
+        public static string AppPath { get; set; } = string.Empty;
+
+        internal const string arg_settings = "/settings";
+        internal const string arg_exit = "/exit-safe";
+        internal const string arg_restore = "/restore";
+        internal const string arg_appupdated = "/app-restarted-after-update";
+        internal const char arg_delimiter = ' ';
+
+        public static void CreateJumpList()
+        {
+            AppPath = Path.Combine(Directory.GetCurrentDirectory(), $"{Program.AppHostName}.exe");
+
+            JumpList jumpList = new();
+            JumpTask settingsTask = new()
+            {
+                Title = Properties.Strings.SettingsItem,
+                Description = Properties.Strings.SettingsItemDescription,
+                ApplicationPath = AppPath,
+                Arguments = arg_settings,
+                IconResourceIndex = -1
+            };
+            jumpList.JumpItems.Add(settingsTask);
+
+            JumpTask restoreTask = new()
+            {
+                Title = Properties.Strings.RestoreDefaultItem,
+                Description = Properties.Strings.RestoreDefaultItemDescription,
+                ApplicationPath = AppPath,
+                Arguments = arg_restore,
+                IconResourceIndex = -1,
+            };
+            jumpList.JumpItems.Add(restoreTask);
+
+            JumpTask exitTask = new()
+            {
+                Title = Properties.Strings.ExitItem,
+                Description = Properties.Strings.ExitItemDescription,
+                ApplicationPath = AppPath,
+                Arguments = arg_exit,
+                IconResourceIndex = -1
+            };
+            jumpList.JumpItems.Add(exitTask);
+
+            jumpList.Apply();
+        }
+    }
+}
