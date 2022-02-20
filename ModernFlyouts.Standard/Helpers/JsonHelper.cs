1500f54b5df60c9282f921fe7c29ce6449bb3864
FireCubeStudios <40955345+FireCubeStudios@users.noreply.github.com>
2022-02-20 15:26:02 +0100

    Audio service and Json helper

---
+++ ModernFlyouts.Standard/Helpers/JsonHelper.cs
@@ -0,0 +1,27 @@
+﻿using ModernFlyouts.Standard.Classes;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModernFlyouts.Standard.Helpers
+{
+    class JsonHelper
+    {
+        public async Task<Settings> DeserializeSettings(string JSON)
+        {
+            return await Task.Run<Settings>(() =>
+            {
+                return JsonConvert.DeserializeObject<Settings>(JSON);
+            });
+        }
+        public async Task<string> SerializeSettings(Settings JSONObj)
+        {
+            return await Task.Run<string>(() =>
+            {
+                return JsonConvert.SerializeObject(JSONObj);
+            });
+        }
+    }
+}
